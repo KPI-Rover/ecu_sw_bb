72101121777546bd787b9526b80e2e48d9c50704
taketook34 <taketook34@gmail.com>
2025-02-21 19:51:39 +0200

    changing tabs (right)

--- tcp-server/include/TCPServerReceiver.h
+++ tcp-server/include/TCPServerReceiver.h
@@ -10,50 +10,50 @@ char* get_primary_ip();
 
 class TCPServer {
 public:
-	char *server_address;
-	int server_portnum;
+    char *server_address;
+    int server_portnum;
 
-	sem_t *progSemaphore;
-	MotorProcessor *commandProcessor;
+    sem_t *progSemaphore;
+    MotorProcessor *commandProcessor;
 
-	TCPServer(const char *ip_address, int port, MotorProcessor *proc, sem_t *sem) {
-		server_address = new char[strlen(ip_address) + 1 ];
-		strcpy(server_address, ip_address);
+    TCPServer(const char *ip_address, int port, MotorProcessor *proc, sem_t *sem) {
+        server_address = new char[strlen(ip_address) + 1 ];
+        strcpy(server_address, ip_address);
 
-		commandProcessor = proc; // set for using motorProcess class inside our tcp server
-		progSemaphore = sem;
+        commandProcessor = proc; // set for using motorProcess class inside our tcp server
+        progSemaphore = sem;
 
-		server_portnum = port;
-	};
-	
-	static void* serverThreadFuncWrapper(void *arg) {
-		reinterpret_cast<TCPServer*>(arg)->serverThreadFunc();
-	    return nullptr;	
-		
-	}
-	
-	static void* timerThreadFuncWrapper(void *arg) {
-		reinterpret_cast<TCPServer*>(arg)->timerThreadFunc();
-	    return nullptr;	
-	}
+        server_portnum = port;
+    };
+    
+    static void* serverThreadFuncWrapper(void *arg) {
+        reinterpret_cast<TCPServer*>(arg)->serverThreadFunc();
+        return nullptr;	
+        
+    }
+    
+    static void* timerThreadFuncWrapper(void *arg) {
+        reinterpret_cast<TCPServer*>(arg)->timerThreadFunc();
+        return nullptr;	
+    }
 
-	int init();
-	void start();
-	void destroy();
-		
+    int init();
+    void start();
+    void destroy();
+        
 
 
 private:
-	pthread_mutex_t timer_mutex = PTHREAD_MUTEX_INITIALIZER;
-	pthread_t serverThread_id, timerThread_id;
-	int sockfd;
+    pthread_mutex_t timer_mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_t serverThread_id, timerThread_id;
+    int sockfd;
 
 
 
-	int get_counter();
-	int counter = get_counter(); // 100 ms - timer precision . I use usleep() function
-	void* serverThreadFunc();
-	void* timerThreadFunc();
+    int get_counter();
+    int counter = get_counter(); // 100 ms - timer precision . I use usleep() function
+    void* serverThreadFunc();
+    void* timerThreadFunc();
 
 };
 
--- tcp-server/include/motorsProcessor.h
+++ tcp-server/include/motorsProcessor.h
@@ -6,71 +6,71 @@
 
 class Motor {
 public:
-	int motorNumber;
-	//int brakeTime;
+    int motorNumber;
+    //int brakeTime;
 
-	
-	Motor(int assignedNumber) {
-		motorNumber = assignedNumber;
-	};	
+    
+    Motor(int assignedNumber) {
+        motorNumber = assignedNumber;
+    };	
 
-	void motorGo(int newRPM) {
-		if (newRPM >= MAX_RPM) {
-			newRPM = MAX_RPM;
-		}
+    void motorGo(int newRPM) {
+        if (newRPM >= MAX_RPM) {
+            newRPM = MAX_RPM;
+        }
 
-		currentRPM = newRPM;
-		//cout << "new rpm" << newRPM << endl; // debugging 
-		currentDutyCycle = getDC(newRPM);
-		if (motorNumber >= MOTOR_INVERTED) {
-			currentDutyCycle = -currentDutyCycle;	
-		}
-		
-		//cout << "go "  << currentDutyCycle  << endl; // debug
+        currentRPM = newRPM;
+        //cout << "new rpm" << newRPM << endl; // debugging 
+        currentDutyCycle = getDC(newRPM);
+        if (motorNumber >= MOTOR_INVERTED) {
+            currentDutyCycle = -currentDutyCycle;	
+        }
+        
+        //cout << "go "  << currentDutyCycle  << endl; // debug
 
-		rc_motor_set(motorNumber, currentDutyCycle);
-	}
+        rc_motor_set(motorNumber, currentDutyCycle);
+    }
 
-	void motorStop() {
-		rc_motor_brake(motorNumber);
-		rc_usleep(BRAKE_TIME);
-		rc_motor_free_spin(motorNumber);
-	}
+    void motorStop() {
+        rc_motor_brake(motorNumber);
+        rc_usleep(BRAKE_TIME);
+        rc_motor_free_spin(motorNumber);
+    }
 
-	int getRPM () {
-		return currentRPM;
-	}
+    int getRPM () {
+        return currentRPM;
+    }
 
 private:	
 
 
-	int currentRPM;
-	double currentDutyCycle;
+    int currentRPM;
+    double currentDutyCycle;
 
 
-	double getDC(int entryRPM) {
-		double result = static_cast<double>(entryRPM) / MAX_RPM;
-		//cout << result << endl; // debug
-		return result;
-	}
+    double getDC(int entryRPM) {
+        double result = static_cast<double>(entryRPM) / MAX_RPM;
+        //cout << result << endl; // debug
+        return result;
+    }
 
 };
 
 class MotorProcessor {
 public:
-	Motor* motors;
+    Motor* motors;
 
 
-	
-	
-	void init(const int* motorsArray);
-	
-	void setMotorRPM(int channel, int newRPM);
-	void stopMotor(int channel);
+    
+    
+    void init(const int* motorsArray);
+    
+    void setMotorRPM(int channel, int newRPM);
+    void stopMotor(int channel);
 
-	int getMotorRPM(int channel);
+    int getMotorRPM(int channel);
 
-	void destroy();
+    void destroy();
 
 
 };
