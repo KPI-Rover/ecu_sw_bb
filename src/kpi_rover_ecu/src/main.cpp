e459e60c475bf1034fcf9dc309271f76119e20bd
taketook34 <taketook34@gmail.com>
2025-03-10 23:44:11 +0200

    Issue #11: Rewriting socket server. Creating ITransport, TCPTransport. Deleted TCPServer

--- src/kpi_rover_ecu/src/main.cpp
+++ src/kpi_rover_ecu/src/main.cpp
@@ -1,13 +1,24 @@
-#include "TCPServerReceiver.h"
+//#include "TCPServerReceiver.h"
+#include "TCPTransport.h"
 #include "config.h"
 #include "motorsController.h"
 #include "protocolHandler.h"
 using namespace std;
 
 
-sem_t stopProgramSem; /// global value of semaphor that is ckecked to stop rpogrma
+//sem_t stopProgramSem; /// global value of semaphor that is ckecked to stop rpogrma
+
+atomic<bool> runningProgram(false);
+atomic<bool> runningState(false);
+atomic<int> counter(0);
 
 void interruptSignalHandler(int signal); 
+int get_counter();
+
+void timerThreadFuction(ProtocolHanlder *workClass);
+
+char* get_primary_ip();
+
 
 int main(int argc, char* argv[]) {
     char *server_address = get_primary_ip();
@@ -30,41 +41,144 @@ int main(int argc, char* argv[]) {
             return EXIT_FAILURE;
         }
     }
-    sem_init(&stopProgramSem, 0, 0);
+    //sem_init(&stopProgramSem, 0, 0);
     
 
     MotorController motors_processor;
     motors_processor.init(SHASSIARR);
     ProtocolHanlder protocolHandler_(&motors_processor);
-    TCPServer server(server_address, server_portnum, &protocolHandler_, &stopProgramSem);
+    counter.store(get_counter());
+    thread timerThread(timerThreadFuction, &protocolHandler_);
 
-    if (server.init()  == -1) {
+    TCPTransport TcpTransport_(server_address, server_portnum);
+
+    if (TcpTransport_.init() == -1) {
         cout << "[ERROR] Error creating socket" << endl;
-        server.destroy();
+        TcpTransport_.destroy();
         return 1;
     }
 
-    if (server.start() == -1) {
-        cout << "[ERROR] Error strting of receiving messages" << endl;
-        server.destroy();
-        return 1;
-    }
+    cout << "start ..." << endl;
 
     signal(SIGINT, interruptSignalHandler); // initializing of custom signal handlers
     signal(SIGTERM, interruptSignalHandler); // initializing of custom signal handlers
 
-    sem_wait(&stopProgramSem);
-    cout << "Stopping ..." << endl;
+    TcpTransport_.start();
 
-    server.destroy();
-    motors_processor.destroy();
+    while (true) {
+        if (runningProgram) {
+            // TcpTransport_.destroy();
+            // motors_processor.destroy();
+            cout << "END of program" << endl;
+            cout << "joining threads" << endl;
+            runningState = true;
+            if (timerThread.joinable()) {
+                timerThread.join();
+            }
+            cout << "destroying drivers" << endl;
+            motors_processor.destroy();
+            TcpTransport_.destroy();
+            return 0;
+            //break;
+        }
+        vector<uint8_t> message;
+        if (TcpTransport_.receive(message)) {
+            counter.store(get_counter());
+            vector<uint8_t> return_message = protocolHandler_.handleMessage(message);
+            TcpTransport_.send(return_message);
+        }
+    }
+
+    
+    // sem_wait(&stopProgramSem);
+    // cout << "Stopping ..." << endl;
+
+    // server.destroy();
+    // motors_processor.destroy();
     
     return 0;
 }
 
+void timerThreadFuction(ProtocolHanlder *workClass) {
+    vector<uint8_t> stopVector;
+    int32_t stopValue  = 0;
+    stopVector.push_back(ID_SET_ALL_MOTORS_SPEED);
+    for (int i = 0; i < MOTORS_NUMBER; i++) {
+        stopVector.insert(stopVector.end(), 
+                      reinterpret_cast<uint8_t*>(&stopValue), 
+                      reinterpret_cast<uint8_t*>(&stopValue) + sizeof(int32_t));
+    }
+
+    while (!runningState) {
+        if (counter > 0) {
+            usleep(TIMERPRECISION);
+            counter--;
+
+            if (counter == 0 ) {
+                cout << "[INFO] Motor set to stop" << endl;
+            }
+
+        } else if (counter == 0) {
+            // command to stop all motors
+            workClass->handleMessage(stopVector);
+
+            sleep(TIMESTOP);
+            
+                
+        }
+            
+    }
+
+
+}
+
+char* get_primary_ip() {
+    /*
+         Automatically finding IP adress for hosting server
+        returns string of IP-address
+     */
+    struct ifaddrs *ifaddr, *ifa;
+    int family, s;
+    char *host = new char[NI_MAXHOST];
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("[ERROR] getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        family = ifa->ifa_addr->sa_family;
+
+        if (family == AF_INET) { // Check it is IPv4
+            s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in),
+                    host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+            if (s != 0) {
+                printf("[ERROR] failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+            // Ignore loopback address
+            if (strcmp(ifa->ifa_name, "lo") != 0) {
+                break;
+            }
+        }
+    }
+
+    freeifaddrs(ifaddr);
+    return host;
+}
+
+int get_counter() {
+    return  (TIMESTOP * ONESECONDMICRO) / TIMERPRECISION;
+}
+
 void interruptSignalHandler(int signal) {
+    
     cout << "Get signal to stop program" << endl;
-    sem_post(&stopProgramSem);
+    runningProgram = true;
+    //sem_post(&stopProgramSem);
     //exit(0);
     
 }
