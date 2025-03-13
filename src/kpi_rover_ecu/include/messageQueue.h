2a46b027b7dc1fad5ab9475773a4521591c2c80f
taketook34 <taketook34@gmail.com>
2025-03-13 13:29:42 +0200

    Issue #11: Adding MessageQueue

---
+++ src/kpi_rover_ecu/include/messageQueue.h
@@ -0,0 +1,22 @@
+#ifndef MESSAGEQUEUE_H
+#define MESSAGEQUEUE_H
+
+#include "config.h"
+
+class MessageQueue {
+public:
+
+	void push(const vector<uint8_t>& msg);
+	bool pop(vector<uint8_t>& msg, int timeout_ms);
+	void clear();
+
+private:
+	queue<vector<uint8_t>> queue_;
+	pthread_mutex_t mutex_ = PTHREAD_MUTEX_INITIALIZER;
+	pthread_cond_t cv_ = PTHREAD_COND_INITIALIZER;
+	//mutex mutex_;
+	//condition_variable cv_;
+};
+
+
+#endif
--- src/kpi_rover_ecu/src/TCPTransport.cpp
+++ src/kpi_rover_ecu/src/TCPTransport.cpp
@@ -1,6 +1,7 @@
 #include "TCPTransport.h"
 #include "ITransport.h"
 #include "config.h"
+#include "messageQueue.h"
 
 TCPTransport::TCPTransport(const char *ip_address, int port) {
 	server_address = new char[strlen(ip_address) + 1 ];
@@ -61,6 +62,20 @@ void TCPTransport::start() {
 			client_sockfd = accept(sockfd, nullptr, nullptr);
 			if (client_sockfd >= 0) {
 				cout << "Client connected." << endl;
+				while (1) {
+					uint8_t buffer[1024];
+					ssize_t bytes_received = recv(client_sockfd, buffer, sizeof(buffer), 0);
+					if (bytes_received > 0) {
+						vector<uint8_t> message(buffer, buffer+bytes_received);
+						messageQueue_.push(message);
+					} else {
+						cout << "Client disconnected." << endl;
+						close(client_sockfd);
+						client_sockfd = -1;
+						break;
+					}
+
+				}
 			}
 		}
 	});
@@ -72,20 +87,8 @@ bool TCPTransport::send(const vector<uint8_t>& data) {
 }
 
 bool TCPTransport::receive(vector<uint8_t>& data) {
-	if (client_sockfd < 0) return false;
+	return messageQueue_.pop(data, 50);
 
-	uint8_t buffer[1024];
-	ssize_t bytes_received = recv(client_sockfd, buffer, sizeof(buffer), 0);
-
-	if (bytes_received > 0) {
-		data.assign(buffer, buffer + bytes_received);
-		return true;
-	} else {
-		cout << "Client disconnected." << endl;
-		close(client_sockfd);
-		client_sockfd = -1;
-		return false;
-	}
 }
 
 void TCPTransport::destroy() {
